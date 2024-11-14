21198510ca899a36e6407aef1602dced707c5293
AA22_Gi <gibert22@mail.ru>
2024-11-14 20:19:18 +0700

    created User

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
